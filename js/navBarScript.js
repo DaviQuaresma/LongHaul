0780c17143eed347dfb45072ac3b008af2d8d027
Davi Quaresma da Silva Henriques <quaresma.davi@gmail.com>
2023-11-28 16:37:52 -0300

    .

--- js/navBarScript.js
+++ js/navBarScript.js
@@ -42,3 +42,20 @@ const mobileNavbar = new MobileNavbar(
   ".nav-list li",
 );
 mobileNavbar.init();
+
+function trocarImagem() {
+  var imagem = document.getElementById('imagem');
+  if (window.innerWidth <= 425) {
+      imagem.innerHTML = '<img class="img-responsive" src="/images/SobreImg/SobreBG.png" alt="">';
+  } else {
+      imagem.innerHTML = '<img class="img-full" src="/images/SobreImg/VictorFace.png" alt="">';
+  }
+}
+
+// Chame a função inicialmente
+trocarImagem();
+
+// Adicione um ouvinte de evento de redimensionamento
+window.addEventListener('resize', function () {
+  trocarImagem();
+});

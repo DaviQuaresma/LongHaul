fd696f716d1b2e7175d2b5f04734807940fc4979
Davi Quaresma da Silva Henriques <quaresma.davi@gmail.com>
2023-12-12 12:01:57 -0300

    Alterações nos names

---
+++ js/changePhoto.js
@@ -0,0 +1,16 @@
+function trocarImagem() {
+    var imagem = document.getElementById('imagem');
+    if (window.innerWidth <= 425) {
+        imagem.innerHTML = '<img class="img-responsive" src="/images/SobreImg/SobreBG.png" alt="">';
+    } else {
+        imagem.innerHTML = '<img class="img-full" src="/images/SobreImg/VictorFace.png" alt="">';
+    }
+  }
+  
+  // Chame a função inicialmente
+  trocarImagem();
+  
+  // Adicione um ouvinte de evento de redimensionamento
+  window.addEventListener('resize', function () {
+    trocarImagem();
+  });

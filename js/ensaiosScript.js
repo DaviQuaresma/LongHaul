e5ab5a7443991903cfea06d26e9de6486bc66fd6
Davi Quaresma da Silva Henriques <quaresma.davi@gmail.com>
2023-12-14 12:07:23 -0300

    Pequenos ajustes na estilização e no script

--- js/ensaiosScript.js
+++ js/ensaiosScript.js
@@ -41,10 +41,11 @@ function scrollImages(direction) {
 function openModal(imageSrc) {
     const modalImage = document.getElementById('modalImage');
     modalImage.style.width = 'auto';
-    modalImage.style.maxWidth = '83%';
-    modalImage.style.maxHeight = '83vh';
-    modalImage.style.marginLeft = '14%';
-    modalImage.style.marginRight = 'auto';
+    modalImage.style.marginLeft = '0%';
+    modalImage.style.maxWidth = '90%';  // Você pode ajustar isso conforme necessário
+    modalImage.style.maxHeight = '90vh';  // Você pode ajustar isso conforme necessário
+    modalImage.style.transform = 'translateX(10%)';
+
 
     // Cria uma nova imagem para verificar as dimensões
     const tempImage = new Image();
@@ -56,26 +57,75 @@ function openModal(imageSrc) {
         // Verifica se a imagem é horizontal
         if (aspectRatio > 1) {
             modalImage.style.width = 'auto';
-            modalImage.style.maxWidth = '750px';
-            modalImage.style.maxHeight = '750px';
-            modalImage.style.marginLeft = '0%';
-            modalImage.style.left = '15%';
-            modalImage.style.transform = 'translateX(-15%)';
-
-            // Ajusta a posição do botão para imagens horizontais
-            const closeButton = document.querySelector('.btn-close');
-            closeButton.style.top = '1em';
-            closeButton.style.right = '17em';
-        } else {
-            // Ajusta a posição do botão para imagens verticais
-            const closeButton = document.querySelector('.btn-close');
-            closeButton.style.top = '0.5em';
-            closeButton.style.right = '30em';
+            modalImage.style.maxWidth = '800px';
+            modalImage.style.maxHeight = '800px';
+            modalImage.style.marginLeft = '20%';
+            // modalImage.style.marginRight = '20%';
+            // modalImage.style.left = '0%';
+            modalImage.style.transform = 'translateX(-30%)';
         }
 
-        modalImage.src = imageSrc;
+        // Obtém a largura da janela
+            var windowWidth = window.innerWidth;
 
-        const modal = new bootstrap.Modal(document.getElementById('imageModal'));
-        modal.show();
-    };
+            // Verifica se a largura da janela é menor ou igual a 440px
+            if (windowWidth <= 440 && aspectRatio > 1) {
+                // Ajusta o estilo do modalImage
+                modalImage.style.width = 'auto';
+                modalImage.style.maxWidth = '290px';
+                modalImage.style.maxHeight = '290px';
+                modalImage.style.marginLeft = '7%';
+                modalImage.style.left = '7%';
+                modalImage.style.transform = 'translateX(-5%)';
+            } 
+
+    modalImage.src = imageSrc;
+
+    const modal = new bootstrap.Modal(document.getElementById('imageModal'));
+    modal.show();
+
+    }
 }
+
+//Nova função para validar proporções
+// function openModal(imageSrc) {
+//     const modalImage = document.getElementById('modalImage');
+//     modalImage.style.width = 'auto';
+//     modalImage.style.maxWidth = '83%';
+//     modalImage.style.maxHeight = '83vh';
+//     modalImage.style.marginLeft = '14%';
+//     modalImage.style.marginRight = 'auto';
+
+//     // Cria uma nova imagem para verificar as dimensões
+//     const tempImage = new Image();
+//     tempImage.src = imageSrc;
+
+//     tempImage.onload = function () {
+//         const aspectRatio = tempImage.width / tempImage.height;
+
+//         // Verifica se a imagem é horizontal
+//         if (aspectRatio > 1) {
+//             modalImage.style.width = 'auto';
+//             modalImage.style.maxWidth = '750px';
+//             modalImage.style.maxHeight = '750px';
+//             modalImage.style.marginLeft = '0%';
+//             modalImage.style.left = '15%';
+//             modalImage.style.transform = 'translateX(-15%)';
+
+//             // Ajusta a posição do botão para imagens horizontais
+//             const closeButton = document.querySelector('.btn-close');
+//             closeButton.style.top = '1em';
+//             closeButton.style.right = '17em';
+//         } else {
+//             // Ajusta a posição do botão para imagens verticais
+//             const closeButton = document.querySelector('.btn-close');
+//             closeButton.style.top = '0.5em';
+//             closeButton.style.right = '30em';
+//         }
+
+//         modalImage.src = imageSrc;
+
+//         const modal = new bootstrap.Modal(document.getElementById('imageModal'));
+//         modal.show();
+//     };
+// }
--- js/navBarScript.js
+++ js/navBarScript.js
@@ -43,3 +43,32 @@ const mobileNavbar = new MobileNavbar(
 );
 mobileNavbar.init();
 
+
+document.addEventListener("DOMContentLoaded", function () {
+  const mobileMenu = document.querySelector('.mobile-menu');
+  const navList = document.querySelector('.nav-list');
+  const logoImage = document.querySelector('.logo');
+
+  // Verificar se a URL da página contém um padrão específico
+  const isGreenLogoPage = window.location.href.includes("/pagina-com-logo-verde");
+
+  // Definir os caminhos padrão e verde para a logo
+  const defaultLogoPath = "/images/HomeImg/LogoBranca.webp";
+  const greenLogoPath = "/images/IndexImg/LogoVerde.webp";
+
+  // Definir a imagem inicial com base na página
+  logoImage.src = isGreenLogoPage ? greenLogoPath : defaultLogoPath;
+
+  mobileMenu.addEventListener('click', function () {
+    mobileMenu.classList.toggle('clicked');
+    navList.classList.toggle('black-menu-active');
+
+    if (mobileMenu.classList.contains('clicked')) {
+      // Se o menu estiver aberto, altere a imagem para a logo verde
+      logoImage.src = greenLogoPath;
+    } else {
+      // Se o menu estiver fechado, restaure a imagem com base na página
+      logoImage.src = isGreenLogoPage ? greenLogoPath : defaultLogoPath;
+    }
+  });
+});
